b6c152c6a7bdca31c32570079154831243e4416b
Parksejin0628 <64259593+Parksejin0628@users.noreply.github.com>
2023-12-26 23:40:29 +0900

    컷신 구현

---
+++ sum i/Assets/02.Scripts/Manager/CutSceneManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+
+public class CutSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int currentCutIndex = 0;
+    public string nextSceneName;
+    public GameObject[] cutScenes;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.anyKeyDown && currentCutIndex < cutScenes.Length)
+        {
+            cutScenes[currentCutIndex].SetActive(false);
+            currentCutIndex++;
+        }
+        if (currentCutIndex == cutScenes.Length)
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+
+    }
+}
