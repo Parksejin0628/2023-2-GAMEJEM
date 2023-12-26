3dbf960c6f9ffb82a657882f82a7b85516e86fd0
Parksejin0628 <64259593+Parksejin0628@users.noreply.github.com>
2023-12-26 18:38:45 +0900

    점프 카운트 제한 구현

--- sum i/Assets/02.Scripts/PlayerCtrl.cs
+++ sum i/Assets/02.Scripts/PlayerCtrl.cs
@@ -11,6 +11,10 @@ public class PlayerCtrl : MonoBehaviour
 
     public float moveSpeed = 3.0f;
     public float jumpPower = 10.0f;
+    public float rayDistance = 0.2f;
+    public int maxJumpCount = 1;
+    public int jumpCount = 1;
+
     Vector2 wasdVector;
 
     void Start()
@@ -27,11 +31,24 @@ public class PlayerCtrl : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
+        if (rigidbody2D.velocity.y < 0 && CheckIsGround(LayerMask.GetMask("Ground"), out RaycastHit2D hit))
+        {
+            jumpCount = maxJumpCount;
+        }
     }
 
     void Move()
     {
         rigidbody2D.velocity = new Vector2(wasdVector.x * moveSpeed, rigidbody2D.velocity.y);
+    }
+    bool CheckIsGround(LayerMask layerMask, out RaycastHit2D hit) //layerMask 태크를 가진 땅인지 체크하는 함수
+    {
+        hit = Physics2D.BoxCast(transform.position + Vector3.down * transform.localScale.y / 4, transform.localScale / 3, 0, Vector2.down, rayDistance, layerMask);
+        if (hit == true)
+        {
+            return true;
+        }
+        return false;
     }
 
     void OnMove(InputValue value)   //방향키를 누르거나 땔 때 호출되는 함수이다.
@@ -43,7 +60,20 @@ public class PlayerCtrl : MonoBehaviour
 
     void OnJump()   //스페이스바를 누르면 호출되는 함수이다.
     {
-        rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpPower);
+        if(CheckIsGround(LayerMask.GetMask("Ground"), out RaycastHit2D hit) && jumpCount == maxJumpCount)
+        {
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpPower);
+            jumpCount--;
+        }
+        else if(jumpCount > 0)
+        {
+            if (jumpCount == maxJumpCount)
+            {
+                jumpCount--;
+            }
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpPower);
+            jumpCount--;
+        }
     }
 
 }
